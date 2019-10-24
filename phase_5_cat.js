c5db2ddd7a02b4b4bd0742105dd2a56ce85d73a2
Ken Klooster <kklooster3910@gmail.com>
2019-10-24 15:56:24 -0400

    leetcode algorithm work

--- phase_5_cat.js
+++ phase_5_cat.js
@@ -71,3 +71,15 @@ Student.prototype.courseLoad = function() {
 // Input: [[1,3],[3,0,1],[2],[0]]
 // Output: false
 // Explanation: We can't enter the room with number 2.
+
+// Input: [[1],[2],[3],[]]
+// Output: true
+// Explanation:  
+// We start in room 0, and pick up key 1.
+// We then go to room 1, and pick up key 2.
+// We then go to room 2, and pick up key 3.
+// We then go to room 3.  Since we were able to go to every room, we return true.
+
+// Input: [[1,3],[3,0,1],[2],[0]]
+// Output: false
+// Explanation: We can't enter the room with number 2.
