fef28848f1f44674726f43dbf7b5f42200b98d84
Ken Klooster <kklooster3910@gmail.com>
2019-10-28 19:18:40 -0400

    Small coffee shop project

---
+++ coffee_shop/dat_koff.js
@@ -0,0 +1,8 @@
+// coffee shop setup
+// one coffee machine, has a brew function -- has bold brew and normal brew
+// types of coffee -- dark, light, and espresso
+// espresso machine takes longe
+// ie: will have one espressso machine
+// espresso machine will take longer, has the milk steamer option and the the espresso press
+// coffee sizes are s, m, and large, 8, 16, and 24 oz ... who the fuck needs 24oz of coffee
+
